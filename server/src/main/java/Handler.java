cca50a219de1c37e3a024c135f0cebffb016de89
AleksandrTimonin <GrAleen@ya.ru>
2021-09-21 05:13:12 +0300

    выполняется работа по уроку

--- server/src/main/java/Handler.java
+++ server/src/main/java/Handler.java
@@ -1,6 +1,6 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class Handler implements Runnable{
     private final Socket soket;
@@ -12,15 +12,22 @@ public class Handler implements Runnable{
     public Socket getSoket() {
         return soket;
     }
+    private final String SINCRONIZED_DIR = "server/root/syncronyzed/";
+    private byte[] buffer = new byte[1024];
 
     @Override
     public void run() {
         try (DataInputStream is = new DataInputStream(soket.getInputStream()); DataOutputStream os = new DataOutputStream(soket.getOutputStream())){
             while (true){
                 String s = is.readUTF();
-                System.out.printf("Recived { %s }" + s);
-                System.out.println();
-                os.writeUTF(s);
+                if(s.startsWith("{F_N}")){
+                    System.out.println("Recived { "+s+" }"  );
+                    s = s.substring(5,s.length());
+                    createOrUpdateFiles(s,is);
+                }
+
+
+                os.writeUTF(s + " сохранён в облаке ");
                 os.flush();
 
             }
@@ -29,5 +36,25 @@ public class Handler implements Runnable{
             e.printStackTrace();
             System.out.println("connection lost");
         }
+    }
+    public void createOrUpdateFiles(String filename, DataInputStream is){
+            File dir = new File(SINCRONIZED_DIR );
+            if(!dir.exists()) dir.mkdir();
+            File file = new File( SINCRONIZED_DIR + filename);
+        try(FileOutputStream fileOutputStream = new FileOutputStream(file)){
+            if(file.exists()){
+                file.delete();
+            }
+            file.createNewFile();
+            String readed = is.readUTF();
+
+                fileOutputStream.write(readed.getBytes(StandardCharsets.UTF_8));
+                fileOutputStream.flush();
+
+            System.out.println("файл сохранён");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
